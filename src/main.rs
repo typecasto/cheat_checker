cf9277ca0c97024729ea8b26dcca15b037b7551f
Larson <larsona@outlook.com>
2023-02-15 06:58:49 -0600

    Add multithreading

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@
 
 use encoding_rs::Encoding;
 use indicatif::ProgressBar;
-use log::LevelFilter::{Info, Debug};
-// use owo_colors::{style, OwoColorize};
+use log::LevelFilter::{Debug, Info};
 use std::collections::{BinaryHeap, HashMap};
 use std::io::Read;
 use std::num::NonZeroUsize;
 use std::path::PathBuf;
+use std::sync::mpsc::Sender;
+use std::sync::{mpsc, Arc, Mutex};
 use std::{process, thread};
 use strsim::{normalized_damerau_levenshtein, normalized_levenshtein};
 
 #[derive(Debug, Clone, bpaf::Bpaf)]
 #[bpaf(options, version)]
-struct CheatCheck {
-
+struct CliArgs {
     /// Lower bound for cheat detection.
     ///
     /// Between 0 and 1, where 1 means identical files.
@@ -22,33 +22,27 @@ struct CheatCheck {
     sensitivity: f32,
 
     /// Number of calculations to run in parallel.
-    /// 
+    ///
     /// The default is 0, meaning autodetect.
     #[bpaf(short, long, argument("N"), fallback(0))]
     jobs: usize,
-    
+
     /// Show additional debugging information.
     #[bpaf(short, long, switch)]
     verbose: bool,
 
     /// Logs all comparisons to this file.
-    #[bpaf(short, long("log"), argument("FILE"))]
+    #[bpaf(short, long("log"), argument("FILE"), hide)]
     logfile: Option<PathBuf>,
-
-    /// Use Damerau-Levenshtein distance instead of Levenshtein distance.
-    ///
-    /// About 20x slower.
-    #[bpaf(short('D'), long("damerau"), switch)]
-    damerau_mode: bool,
 
     /// Program used to format code before checking
     ///
     /// Before comparing two files, we'll run them both through this program.
     /// Improves detection, since changing the format won't affect the results
     /// anymore.
-    /// 
+    ///
     /// TODO
-    #[bpaf(short, long, argument("PROGRAM"))]
+    #[bpaf(short, long, argument("PROGRAM"), hide)]
     formatter: Option<String>,
 
     /// Ignored file for cheat detection.
@@ -56,9 +50,9 @@ struct CheatCheck {
     /// If a file matches this file exactly, it will not be cheat checked.
     /// This is intended to avoid the situation where several students didn't
     /// do the assignment, and thus have exactly the same file turned in.
-    /// 
+    ///
     /// TODO
-    #[bpaf(short, long, argument("FILE"))]
+    #[bpaf(short, long, argument("FILE"), hide)]
     template: Option<PathBuf>,
 
     /// Files or globs of files to compare.
@@ -68,27 +62,27 @@ struct CheatCheck {
 
 /// Takes a list of paths and turns them into paths matching files
 fn filter_paths(globs: &Vec<String>) -> Vec<PathBuf> {
-    let mut all_files: Vec<PathBuf> = Vec::new();
-    for glob in globs {
-        let paths = glob::glob(glob); // i enjoy this line
+    let mut files: Vec<PathBuf> = Vec::new();
+    for pattern in globs {
+        let paths = glob::glob(pattern);
         match paths {
             Ok(paths) => {
-                let count = all_files.len();
-                all_files.extend(paths.filter_map(Result::ok));
-                if count == all_files.len() {
-                    log::warn!("\"{}\" didn't match any files.", &glob);
+                let count = files.len();
+                files.extend(paths.filter_map(Result::ok));
+                if count == files.len() {
+                    log::warn!("\"{}\" didn't match any files.", &pattern);
                 }
             }
             Err(err) => {
                 log::warn!(
                     "\"{}\" is not a valid pattern, and will be ignored. ({})",
-                    &glob,
+                    &pattern,
                     &err.msg
                 );
             }
         }
     }
-    globs
+    files
         .iter()
         .map(std::fs::canonicalize)
         .filter_map(Result::ok)
@@ -96,39 +90,36 @@ fn filter_paths(globs: &Vec<String>) -> Vec<PathBuf> {
 }
 
 /// Loads a file to a string, handling non-utf-8 encoding
-fn load_file(path: &PathBuf, program: &CheatCheck) -> anyhow::Result<String> {
+fn load_file(path: &PathBuf, program: &CliArgs) -> anyhow::Result<String> {
     let mut file = std::fs::File::open(path)?;
     let mut bytes = Vec::new();
     file.read_to_end(&mut bytes);
     let encoding = chardet::detect(&bytes).0;
     let encoding = Encoding::for_label(encoding.as_bytes()).unwrap_or(encoding_rs::UTF_8);
-    //TODO preprocessing
     Ok(encoding.decode(&bytes).0.to_string())
-}
-
-// compares two paths and gives a float result
-fn compare(x: &str, y: &str, opts: &CheatCheck) -> anyhow::Result<f64> {
-    if opts.damerau_mode {
-        return Ok(normalized_damerau_levenshtein(x, y));
-    } else {
-        return Ok(normalized_levenshtein(x, y));
-    }
 }
 
 fn main() {
     // --- Process arguments and file list
-    let mut opts = cheat_check().run();
+    let mut opts = cli_args().run();
+    // autodetect parallelism if set to 0
     if opts.jobs == 0 {
         opts.jobs = thread::available_parallelism()
-            .unwrap_or(NonZeroUsize::new(1).unwrap()).into();
+            .unwrap_or(NonZeroUsize::new(1).unwrap())
+            .into();
     }
     let opts = opts;
-    let mut paths = filter_paths(&opts.files);
+    // initialize logger based on chosen debug level
     if opts.verbose {
-        pretty_env_logger::formatted_builder().filter_level(Debug).init();
+        pretty_env_logger::formatted_builder()
+            .filter_level(Debug)
+            .init();
     } else {
-        pretty_env_logger::formatted_builder().filter_level(Info).init();
+        pretty_env_logger::formatted_builder()
+            .filter_level(Info)
+            .init();
     }
+    let mut paths = filter_paths(&opts.files);
     // make sure we have enough files
     if paths.len() <= 1 {
         log::error!("Got {} files to compare, need at least 2.", paths.len());
@@ -136,20 +127,19 @@ fn main() {
     } else {
         log::info!("Got {} files to compare.", paths.len())
     }
-    
+
     // --- Compare files
-    // load all files into memory beforehand
+    // preload all files into memory
     let mut files: HashMap<PathBuf, String> = HashMap::new();
     for path in &paths {
         files.insert(path.clone(), load_file(path, &opts).unwrap());
     }
-    
+
     // hashmap for storing scores
     let mut scores: HashMap<(PathBuf, PathBuf), f64> = HashMap::new();
-    
-    // setup workqueue for threads
-    let mut workqueue: Vec<(&PathBuf, &PathBuf)> = Vec::new();
 
+    // queue of comparisons that need to be made
+    let mut workqueue: Vec<(&PathBuf, &PathBuf)> = Vec::new();
     for (x, fx) in &files {
         for (y, fy) in &files {
             // skip this comparison if we've already compared the two in opposite direction
@@ -158,21 +148,77 @@ fn main() {
                 continue;
             }
             workqueue.push((x, y));
-            // let similarity = compare(&fx, &fy, &opts).unwrap();
-            // if &similarity >= &opts.sensitivity.into() {
-                // bar.println(format!(
-                //     "{}\n{}\n\t{:0<1.3}",
-                //     x.as_os_str().to_string_lossy(),
-                //     y.as_os_str().to_string_lossy(),
-                //     similarity.black().on_red()
-                // ));
-            // }
         }
     }
+
+    let mut workqueue: Arc<Mutex<Vec<(&PathBuf, &PathBuf)>>> = Arc::new(Mutex::new(workqueue));
+    // channel for receiving results
+
+    // spawn the threads
+    thread::scope(|scope| {
+        let (tx, rx) = mpsc::channel();
+        let job_count = workqueue.lock().unwrap().len();
+        // worker threads
+        for x in 0..opts.jobs {
+            let workqueue = workqueue.clone();
+            let tx = tx.clone();
+            // give the thread a name in case we have to debug specific threads later
+            thread::Builder::new()
+                .name(x.to_string())
+                .spawn_scoped(scope, || work(workqueue, &files, tx, &opts));
+        }
+        // other thread
+        scope.spawn(move || {
+            let bar = ProgressBar::new(job_count as u64);
+            // loop runs once per message from the worker threads (blocking while waiting)
+            // and ends when all worker threads drop their Senders.
+            for (x, y, score) in rx.iter() {
+                scores.insert((x.clone(), y.clone()), score);
+                if score >= opts.sensitivity as f64 {
+                    // keep this import scoped small, otherwise everything gets
+                    // a billion color methods.
+                    use owo_colors::OwoColorize;
+                    bar.println(format!(
+                        "{}\n{}\n\t{}",
+                        x.to_string_lossy(),
+                        y.to_string_lossy(),
+                        score.on_red()
+                    ));
+                }
+                bar.inc(1);
+            }
+            bar.finish();
+        });
+    });
+}
+
+/// Make comparisons until the workqueue is empty
+fn work<'a>(
+    jobs: Arc<Mutex<Vec<(&'a PathBuf, &'a PathBuf)>>>,
+    files: &HashMap<PathBuf, String>,
+    results: Sender<(&'a PathBuf, &'a PathBuf, f64)>,
+    opts: &CliArgs,
+) {
+    loop {
+        // lock() blocks the thread, the Result is just for if the mutex is poisoned
+        let job = jobs.lock().unwrap().pop();
+        match job {
+            None => break,
+            Some((x, y)) => {
+                let fx = files.get(x).unwrap();
+                let fy = files.get(y).unwrap();
+                results.send((x, y, normalized_levenshtein(fx, fy)));
+            }
+        }
+    }
+    log::debug!(
+        "Worker thread {} exited.",
+        thread::current().name().unwrap()
+    );
 }
 
 #[cfg(test)]
 #[test]
 fn check_opts() {
-    cheat_check().check_invariants(true);
+    cli_args().check_invariants(true);
 }
